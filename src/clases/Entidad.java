30552b5b3b8fcfae3c6b7f2f2bd954d9c7c2c25b
Martin Novoa <martinnovoar2@gmail.com>
2021-09-30 12:22:16 -0300

    Clase entidad

--- src/clases/Entidad.java
+++ src/clases/Entidad.java
@@ -1,4 +1,39 @@
 package clases;
 
 public class Entidad {
+    protected int x;
+    protected int y;
+    private int ancho;
+    private int alto;
+    private String imagen;
+
+    public Entidad(int x, int y, int ancho, int alto, String imagen) {
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+        this.imagen = imagen;
+    }
+
+    public void moverse(int d) {
+        this.x += d;
+    }
+
+    public boolean colisionaCon(Entidad otro) {
+        boolean colisionaEnX = this.x + this.x >= otro.x && otro.x + otro.x >= this.x;
+        boolean colisionaEnY = this.y + this.y >= otro.y && otro.y + otro.y >= this.y;
+        return colisionaEnX && colisionaEnY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
 }
