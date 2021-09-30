592c8c6b3467d9fe7e59a8aa4d9440056c616a4c
Martin Novoa <martinnovoar2@gmail.com>
2021-09-30 12:22:56 -0300

    Planteo de clase jugador

--- src/clases/Jugador.java
+++ src/clases/Jugador.java
@@ -1,4 +1,31 @@
 package clases;
 
-public class Jugador extends Entidad{
+import javax.swing.*;
+
+public class Jugador extends Entidad {
+    private boolean vivo;
+    private String nombre;
+    //private int punto
+    //private int idUsuario
+
+    public Jugador(int x, int y, int ancho, int alto, String imagen) {
+        super(x, y, ancho, alto, imagen);
+    }
+
+    public void saltar() {
+        //TODO
+    }
+
+    public void agacharse() {
+        //TODO
+    }
+
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
