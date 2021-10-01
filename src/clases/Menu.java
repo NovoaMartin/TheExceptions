47259199d1cdd9e30883782d47b89d87df9e5a02
Julian Castellana <59575010+Caste-Coder@users.noreply.github.com>
2021-10-01 20:29:27 -0300

    metodos que estan en uml

--- src/clases/Menu.java
+++ src/clases/Menu.java
@@ -3,4 +3,28 @@ package clases;
 public class Menu {
 	
 	int entero;
+	
+	public void iniciarSesion() {
+		
+	}
+	
+	public void cerrarSesion() {
+		
+	}
+	
+	public void crearSala() {
+		
+	}
+	
+	public void unirseSala() {
+		
+	}
+	
+	public void salirSala() {
+		
+	}
+	
+	public void verListaSalas() {
+		
+	}
 }
