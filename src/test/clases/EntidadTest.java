c29564f3cfbafc6c3a415c13b69b5939e3a8c5e9
franco33333 <francoripamonti1@gmail.com>
2021-10-01 20:07:52 -0300

    Pruebas Partida Test

--- src/test/clases/EntidadTest.java
+++ src/test/clases/EntidadTest.java
@@ -1,8 +1,5 @@
 package test.clases;
 
-import clases.Entidad;
-import org.junit.*;
-
 public class EntidadTest {
 
 
--- src/test/clases/PartidaTest.java
+++ src/test/clases/PartidaTest.java
@@ -1,4 +1,41 @@
 package test.clases;
 
+import java.io.FileNotFoundException;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import clases.Partida;
+
 public class PartidaTest {
+	
+	Partida partida;
+	
+	@Before
+	public void setup() throws FileNotFoundException {
+		partida = new Partida(1, 4000, "mapaTest.in", 4);
+	}
+	
+	@Test
+	public void verificarCantJugadoresTotales() {
+		assertEquals(4, partida.getCantJugadoresTotales());
+	}
+	
+	@Test
+	public void verificarCantJugadoresActivos() {
+		assertEquals(4, partida.getCantJugadoresActivos());
+	}
+	
+	@Test
+	public void verificarCantObstaculos() {
+		assertEquals(4, partida.getCantObstaculos());
+	}
+	
+	@Test
+    public void verificarColisionesTest() {
+        partida.verificarColisiones();
+        assertEquals(0, partida.getCantJugadoresActivos());
+        assertEquals(4, partida.getCantJugadoresTotales());
+    }
 }
