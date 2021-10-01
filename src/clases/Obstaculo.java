b691535008bd50b4fd34a2178ce063f133bc4fd6
Martin Novoa <martinnovoar2@gmail.com>
2021-10-01 15:23:31 -0300

    Aclaracion test obstaculos

--- src/clases/Obstaculo.java
+++ src/clases/Obstaculo.java
@@ -1,7 +1,10 @@
 package clases;
 
+/**
+ * Por el momento esta clase solo necesita la funcionalidad dada por Entidad
+ * Puede cambiar en el futuro
+ */
 public class Obstaculo extends Entidad{
-
 	public Obstaculo(int x, int y, int ancho, int alto, String imagen) {
 		super(x, y, ancho, alto, imagen);
 	}
--- src/test/clases/ObstaculoTest.java
+++ src/test/clases/ObstaculoTest.java
@@ -1,4 +1,5 @@
 package test.clases;
 
+//Innecesario por el momento, 100% code coverage
 public class ObstaculoTest {
 }
