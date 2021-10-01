a0e7c29c7680cb1df4708665c5bc8b2064b24416
Martin Novoa <martinnovoar2@gmail.com>
2021-10-01 18:21:45 -0300

    Arreglo inicializacion de arraylist en Partida

--- src/clases/Partida.java
+++ src/clases/Partida.java
@@ -10,15 +10,17 @@ public class Partida {
     private int velocidad;
     private long tiempo;
 
-    ArrayList<Jugador> jugadoresActivos;
-    ArrayList<Jugador> jugadoresTotales;
-    ArrayList<Obstaculo> obstaculos;
+    private ArrayList<Jugador> jugadoresActivos;
 
+    private ArrayList<Jugador> jugadoresTotales;
+    private ArrayList<Obstaculo> obstaculos;
 
     public Partida(int velocidad, long tiempo, String mapa, int cantidadJugadores) throws FileNotFoundException {
         this.velocidad = velocidad;
         this.tiempo = tiempo;
         this.cargarObstaculos(mapa);
+        this.jugadoresActivos = new ArrayList<>();
+        this.jugadoresTotales = new ArrayList<>();
 
         for (int i = 1; i <= cantidadJugadores; i++) {
             Jugador jugador = new Jugador(0, 0, 5, 10, "dino.jpg", "dino#" + i);
@@ -28,7 +30,7 @@ public class Partida {
     }
 
     //Esto tiene que ejecutarse cada frame del juego
-    private void verificarColisiones() {
+    public void verificarColisiones() {
         Iterator<Jugador> iterator = jugadoresActivos.iterator();
 
         while (iterator.hasNext()) {
@@ -42,7 +44,8 @@ public class Partida {
         }
     }
 
-    private void cargarObstaculos(String nombreArchivo) throws FileNotFoundException {
+    public void cargarObstaculos(String nombreArchivo) throws FileNotFoundException {
+        this.obstaculos = new ArrayList<>();
         Scanner scanner = new Scanner(new File("Archivos/entrada/" + nombreArchivo));
         while (scanner.hasNextInt()) {
             int x = scanner.nextInt();
@@ -53,4 +56,17 @@ public class Partida {
             obstaculos.add(new Obstaculo(x, y, alto, ancho, imagen));
         }
     }
+
+    public int getCantJugadoresActivos() {
+        return this.jugadoresActivos.size();
+    }
+
+    public int getCantJugadoresTotales() {
+        return this.jugadoresTotales.size();
+    }
+
+    public int getCantObstaculos() {
+        return this.obstaculos.size();
+    }
+
 }
